9be2d8ac28a5ec7782cf62b82423ab5cc34e880a
TOLU_SURFACE\toluw <toluwanimi2@gmail.com>
2024-07-07 10:40:50 -0400

    began adding the slideshow for first page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import img1 from '../Assets/img1.jpg'
+import img2 from '../Assets/img2.jpg'
+import img3 from '../Assets/img3.jpg'
 //import {HiArrowNarrowRight} from 'react-icons/hi';
 //import {Link} from 'react-scroll';
 
 const Home = () =>{
     return(
         <div name='Home' className='w-full h-screen bg-[#a34848]'>
-            
+            <div name='Slide container' className='mx-auto px-8 flex flex-auto justify-center h-full w-full'>
+                <div name='Slide 1'>
+                <img src={img1} style={{width: '1000px'}}/>
+                </div>
+                <div name='Slide 2'>
+                <img src={img2} style={{width: '1000px'}}/>
+                </div>
+                <div name='Slide 3'>
+                <img src={img3} style={{width: '1000px'}}/>
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+            </div>
         </div>
     )
 }
