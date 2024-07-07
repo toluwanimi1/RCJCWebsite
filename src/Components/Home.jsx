2323a926243a40d83d361554df1a521cefdef0e1
Tolu <toluwanimi2@gmail.com>
2024-07-07 19:28:42 -0400

    t

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,18 +9,23 @@ const Home = () =>{
     return(
         <div name='Home' className='w-full h-screen bg-[#a34848]'>
             <div name='Slide container' className='mx-auto px-8 flex flex-auto justify-center h-full w-full'>
-                <div name='Slide 1'>
+                <div name='Slide 1' className='fade'>
                 <img src={img1} style={{width: '1000px'}}/>
                 </div>
-                <div name='Slide 2'>
+                <div name='Slide 2' className='fade'>
                 <img src={img2} style={{width: '1000px'}}/>
                 </div>
-                <div name='Slide 3'>
+                <div name='Slide 3'className='fade'>
                 <img src={img3} style={{width: '1000px'}}/>
 
                 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
                 <a class="next" onclick="plusSlides(1)">&#10095;</a>
                 </div>
+            </div>
+            <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
             </div>
         </div>
     )
