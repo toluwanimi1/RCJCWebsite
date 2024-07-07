6fc015dea55f86cd185d185055cdb036dee4527c
TOLU_SURFACE\toluw <toluwanimi2@gmail.com>
2024-07-07 19:32:55 -0400

    fixing some white screen error

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,22 +12,14 @@ const Home = () =>{
                 <div name='Slide 1' className='fade'>
                 <img src={img1} style={{width: '1000px'}}/>
                 </div>
-                <div name='Slide 2' className='fade'>
-                <img src={img2} style={{width: '1000px'}}/>
-                </div>
-                <div name='Slide 3'className='fade'>
-                <img src={img3} style={{width: '1000px'}}/>
+                
 
                 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
                 <a class="next" onclick="plusSlides(1)">&#10095;</a>
                 </div>
             </div>
-            <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
-            </div>
-        </div>
+    
+       
     )
 }
 
