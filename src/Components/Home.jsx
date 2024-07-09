9cfbdd14e9d7aae72444e0585dd7223a64ebde47
Tolu <toluwanimi2@gmail.com>
2024-07-09 07:17:15 -0400

    added image slider currently  trying  to add transition effect

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
 import img1 from '../Assets/img1.jpg'
 import img2 from '../Assets/img2.jpg'
 import img3 from '../Assets/img3.jpg'
-//import {HiArrowNarrowRight} from 'react-icons/hi';
-//import {Link} from 'react-scroll';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
-const Home = () =>{
+const Home = () =>{ 
+
+    const imgslides =[
+        {url:img1},{url:img2},{url:img3},
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? imgslides.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+    };
+  
+    const nextSlide = () => {
+      const isLastSlide = currentIndex === imgslides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    };
     return(
         <div name='Home' className='w-full h-screen bg-[#a34848]'>
-            <div name='Slide container' className='mx-auto px-8 flex flex-auto justify-center h-full w-full'>
-                <div name='Slide 1' className='fade'>
-                <img src={img1} style={{width: '1000px'}}/>
-                </div>
-                
-
-                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                </div>
-            </div>
-    
-       
+             <div className='max-w-[1400px] h-[780px] w-full m-auto py-24 px-4 relative group'>
+           <div
+        style={{ backgroundImage: `url(${imgslides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover transition-all duration-500'
+      ></div>
+       {/* Left Arrow */}
+       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+           </div>
+        </div>
+ 
     )
 }
 
